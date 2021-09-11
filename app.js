f9abafef7f8cde273f365e0e236f1cd5104b18a7
AbdulQadir97 <aq201541@gmail.com>
2021-09-11 21:33:10 +0500

    commit

--- app.js
+++ app.js
@@ -32,11 +32,11 @@ const memberLogin = () => {
     let userLogEmail = document.getElementById("LogUserEmail").value
     let userLogPass = document.getElementById("LogUserPassword").value;
     let loginData = {
-      LoginName: userLogEmail,
+      LoginEmail: userLogEmail,
       LoginPass: userLogPass
     }
     let LoggedUser = retrieveLoginData.filter((v) => {
-      if (v.useremail === loginData.LoginName && v.userpassword === loginData.LoginPass ) { 
+      if (v.useremail === loginData.LoginEmail && v.userpassword === loginData.LoginPass ) { 
         window.location = "./profile.html"
         return v
       }
@@ -49,16 +49,56 @@ const memberLogin = () => {
   
   }
 
+  const createTeam = () => {
+
+    let teamName = document.getElementById('teamName').value
+    let teamCategory = document.getElementById('teamCategory').value
+    let memberNames = [document.getElementById('memberName').value]
+    let memberEmails = [document.getElementById('memberEmails').value]
+    memberNames.toString().split(',');
+    memberEmails.toString().split(',')
+  
+    let teamLeaderEmail = JSON.parse(sessionStorage.getItem('LoggedUser'))
+    if(teamName === "" || teamCategory === "" || memberEmails === "")
+    {
+        alert("Please Fill the Fields First!")
+    }
+    else
+    {
+        let teamData = {teamLeader: teamLeaderEmail[0].useremail, name: teamName, category: teamCategory, membername: memberNames, memberemail: memberEmails, teamPass:teamLeaderEmail[0].useremail}
+        let teamDataStored = localStorage.getItem('Teams')
+
+    if (teamDataStored === null) {
+       teamDataStored = [];
+    }
+    else {
+      teamDataStored = JSON.parse(teamDataStored)
+  
+  
+    }
+    teamDataStored.push(teamData)
+  
+    localStorage.setItem('Teams', JSON.stringify(teamDataStored));
+  }
+}
+
   const showProfile = () => {
 
     let dataReceived = JSON.parse(sessionStorage.getItem('LoggedUser'))
-    let displayContent = document.getElementById('content');
-    displayContent.innerHTML = `USER NAME: ${dataReceived[0].username} </br>  Email: ${dataReceived[0].useremail} </br>  USER PASSWORD: ${dataReceived[0].userpassword}`;
-    console.log(dataReceived)
-  
-  
+    let teamDataReceived = JSON.parse(localStorage.getItem('Teams'))
+    if(dataReceived[0].useremail === teamDataReceived[0].teamLeader)
+    {
+      for(let i = 0; i <= teamDataReceived.length; i++){
+      let displayContent = document.getElementById('Owncontent');
+      let mainContent = document.createElement('div')
+      mainContent.classList.add('col-md-12', 'mt-2', 'profileRow')
+      mainContent.innerHTML = `<h4> ${teamDataReceived[i].category} </h4> <b>members:</b> ${teamDataReceived[i].membername }`;
+      displayContent.appendChild(mainContent)
+      }
+    }
   }
-
+ 
+  
   const logout = ()=> {
     sessionStorage.clear('LoggedUser')
     window.location = "./login.html"
