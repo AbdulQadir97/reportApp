95d482a7b062ba27eaa0bd9d08eeccf2fc53ca15
AbdulQadir97 <aq201541@gmail.com>
2021-09-13 20:53:30 +0500

    commit

--- app.js
+++ app.js
@@ -2,28 +2,22 @@ const dataRegister = () => {
   let userName = document.getElementById('RegUserName').value;
   let userEmail = document.getElementById('RegUserEmail').value;
   let userPassword = document.getElementById('RegUserPassword').value;
-  if(userName === "" || userEmail === "" || userPassword === "")
-  {
-      alert("Please Fill the Fields First!")
-  }
-  else
-  {
-  let userData = { username: userName, useremail: userEmail, userpassword: userPassword }
-  let storedData = localStorage.getItem('RegisterUsers')
-
-  if (storedData === null) {
-      storedData = [];
+  if (userName === "" || userEmail === "" || userPassword === "") {
+    alert("Please Fill the Fields First!")
   }
   else {
+    let userData = { username: userName, useremail: userEmail, userpassword: userPassword }
+    let storedData = localStorage.getItem('RegisterUsers')
+    if (storedData === null) {
+      storedData = [];
+    }
+    else {
       storedData = JSON.parse(storedData)
-
-
+    }
+    storedData.push(userData)
+    localStorage.setItem('RegisterUsers', JSON.stringify(storedData));
+    window.location = "./login.html"
   }
-  storedData.push(userData)
-
-  localStorage.setItem('RegisterUsers', JSON.stringify(storedData));
-  window.location = "./login.html"
-}
 }
 
 const memberLogin = () => {
@@ -36,25 +30,15 @@ const memberLogin = () => {
     LoginPass: userLogPass
   }
   let LoggedUser = retrieveLoginData.filter((v) => {
-    if (v.useremail === loginData.LoginEmail && v.userpassword === loginData.LoginPass ) { 
+    if (v.useremail === loginData.LoginEmail && v.userpassword === loginData.LoginPass) {
       window.location = "./profile.html"
       return v
     }
-  
+
   });
-  // let teamRetrieveData = JSON.parse(localStorage.getItem('Teams'))
-  // let teamLoggedUser = teamRetrieveData.filter((obj) => {
-   
-  //      return obj;
-  // }
-  // )
-  // for(let j=0; j > teamLoggedUser.length; j++)
-  // {
-  //   let emails = teamLoggedUser[j].memberEmails;
-  //   console.log(emails)
-  // } 
-  console.log(LoggedUser)
+
   sessionStorage.setItem('LoggedUser', JSON.stringify(LoggedUser))
+
 
 
 }
@@ -63,72 +47,53 @@ const createTeam = () => {
 
   let teamName = document.getElementById('teamName').value
   let teamCategory = document.getElementById('teamCategory').value
-  // let memberNames = document.getElementById('memberName').value
-  // let memberEmails = document.getElementById('memberEmails').value
-  // memberNames.toString();
-  // memberNames = memberNames.split(",");
-  // memberEmails.toString();
-  // memberEmails =  memberEmails.split(",")
-
   let teamLeaderData = JSON.parse(sessionStorage.getItem('LoggedUser'))
-  if(teamName === "" || teamCategory === "")
-  {
-      alert("Please Fill the Fields First!")
-  }
-  else
-  {
-      let teamData = {teamLeader: teamLeaderData[0].username, name: teamName, category: teamCategory, teamPass:teamLeaderData[0].useremail}
-      let teamDataStored = localStorage.getItem('Teams')
-    
-  if (teamDataStored === null) {
-     teamDataStored = [];
+  if (teamName === "" || teamCategory === "") {
+    alert("Please Fill the Fields First!")
   }
   else {
-    teamDataStored = JSON.parse(teamDataStored)
+    let teamData = { teamLeader: teamLeaderData[0].username, name: teamName, category: teamCategory, teamPass: teamLeaderData[0].useremail }
+    let teamDataStored = localStorage.getItem('Teams')
 
+    if (teamDataStored === null) {
+      teamDataStored = [];
+    }
+    else {
+      teamDataStored = JSON.parse(teamDataStored)
+
+
+    }
+    teamDataStored.push(teamData)
+
+    localStorage.setItem('Teams', JSON.stringify(teamDataStored));
 
   }
-  teamDataStored.push(teamData)
-
-  localStorage.setItem('Teams', JSON.stringify(teamDataStored));
-}
 }
 
 const showProfile = () => {
 
-  let dataReceived = JSON.parse(sessionStorage.getItem('LoggedUser'))
-  let teamDataReceived = JSON.parse(localStorage.getItem('Teams'))
-  if(dataReceived[0].username === teamDataReceived[0].teamLeader)
-  {
-    
-    console.log(dataReceived[0].username + teamDataReceived[0].teamLeader)
-    for(let i = 0; i < teamDataReceived.length; i++){
-    let displayContent = document.getElementById('Owncontent');
-    let mainContent = document.createElement('div')
-    mainContent.classList.add('col-md-12', 'mt-2', 'profileRow')
-    mainContent.innerHTML = `<h4> ${teamDataReceived[i].category} </h4> <b>members:</b> ${teamDataReceived[i].teamLeader }`;
-    displayContent.appendChild(mainContent)
-    }
-  }
-//   for(let k = 0; k<=teamDataReceived.teamLeader.length; k++)
-//   {
-//     if(dataReceived[0].username === teamDataReceived[k].teamLeader)
-//     {
-//       for(let l = 0; l <= teamDataReceived.length; l++){
-//       let displayContent = document.getElementById('Partcontent');
-//       let mainContent = document.createElement('div')
-//       mainContent.classList.add('col-md-12', 'mt-2', 'profileRow')
-//       mainContent.innerHTML = `<h4> ${teamDataReceived[l].category} </h4> <b>members:</b> ${teamDataReceived[l].teamLeader}`;
-//       displayContent.appendChild(mainContent)
+  ownTeam()
 
-//     }
-//   }
-//   window.location = "./profile.html"
-// }
 
- }
-const logout = ()=> {
+}
+
+const logout = () => {
   sessionStorage.clear('LoggedUser')
   window.location = "./login.html"
+}
+
+const ownTeam = () => {
+  let loggedUser = JSON.parse(sessionStorage.getItem('LoggedUser'))
+  let getTeamData = JSON.parse(localStorage.getItem('Teams'))
+  for (const teamData of getTeamData) {
+
+    if (loggedUser[0].username === teamData.teamLeader) {
+      let displayContent = document.getElementById('Owncontent');
+      let mainContent = document.createElement('div')
+      mainContent.classList.add('col-md-12', 'mt-2', 'profileRow')
+      mainContent.innerHTML = `<h4> ${teamData.category} </h4> <b>members:</b> ${teamData.teamLeader}`;
+      displayContent.appendChild(mainContent)
+    }
+  }
 }
 
